3c1b39aa50be851051aa10e3447b659fecb8adc9
Didier Matos <d.matostorres@gmail.com>
2019-08-29 20:44:39 -0500

    Agregando metodo algoritmo y metodo impirmir, ademas de agregar un arreglo string con 8 valores que seran tomados para pasar por algoritmo

--- src/Com/Ndevelopers/Didier/Proceso.java
+++ src/Com/Ndevelopers/Didier/Proceso.java
@@ -1,9 +1,41 @@
 package Com.Ndevelopers.Didier;
 
+import java.util.Scanner;
+ import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  *
  * @author DidierMatos
  */
-public class Proceso {
+
+    public class Proceso {
+
+        String input[] = {"0-0","1-5","2-7","4-8","3-6","7-1","5-3","6-4","8-4"};
+        int tablero[][] = new int[8 + 1][8 + 1];
+
+        public void Principal() { 
+
+            Algoritmo(tablero);
+
+            Imprime(tablero);
+
+        }
     
-}
+        public void Algoritmo(int tab[][] ){    
+
+
+
+
+        }
+
+        public void Imprime(int tablero_final[][]) {      
+
+
+        }
+    
+    }
+
+
+
